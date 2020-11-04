4680de071f1cadef123e000fa832512b4c5f5196
F <9246068@qq.com>
2020-11-04 01:40:30 +0100

    First Commit

---
+++ src/main/java/com/springboot03/demo/controller/HelloController.java
@@ -0,0 +1,26 @@
+package com.springboot03.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello world";
+    }
+
+    // 查数据，在页面展示
+    @RequestMapping("/success")
+    public String sucess(Map<String, Object> map){
+        map.put("hello", "你好");
+        return "success";
+    }
+
+
+}
